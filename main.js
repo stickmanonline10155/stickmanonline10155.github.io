e0e09c191ac3579e764259f77e759fe2466d4081
stickmanonline10155 <kisiren210502@gmail.com>
2022-05-26 01:07:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 var links = ["https://truth.bahamut.com.tw/s01/202111/5582b30853e5b530252b7cf9b61adbc3.JPG",
 
-"https://static1.squarespace.com/static/55de0d46e4b0dff050052f72/t/56c646c29f72661d8c50da5e/1455834820651/NovelBrewingCo_logo_final_whiteBkg_Black.png",
+"https://media.istockphoto.com/vectors/sketch-of-three-books-vector-id803118460?k=20&m=803118460&s=170667a&w=0&h=W2d89oBSHiaR9ocn6T2H-reZacrqFG3323KgwxHMtfE=",
 
 "https://img.technews.tw/wp-content/uploads/2017/03/24101829/7270219_6d3f41bc71_b-e1490321917660.jpg",
 
-"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Western_concert_flute_%28Yamaha%29.jpg/1200px-Western_concert_flute_%28Yamaha%29.jpg",
-"https://media.gq.com.tw/photos/6157f8961dbe40d77bd3a055/16:9/w_2560%2Cc_limit/GettyImages-1136354222.jpg"]             
+"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Western_concert_flute_%28Yamaha%29.jpg/1200px-Western_concert_flute_%28Yamaha%29.jpg", 
+             
+"https://i.pinimg.com/originals/ae/ff/8b/aeff8b4c58c943b5f605da8e9de86def.jpg", 
+             
+"https://media.gq.com.tw/photos/6157f8961dbe40d77bd3a055/16:9/w_2560%2Cc_limit/GettyImages-1136354222.jpg", 
+             
+"https://assets3.thrillist.com/v1/image/2692359/1200x630/flatten;crop_down;webp=auto;jpeg_quality=70"]             
 
-var titles = ["Music", "Novel", "GOchess", "Flute", "Podcast"]
+var titles = ["Music", "Novel", "GOchess", "Flute", "Skateboard", "Podcast", "Darts"]
 
 var index = 0;
 var autoplay = true;
